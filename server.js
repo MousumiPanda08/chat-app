55fee56530b3540c987c4da6546c9a2b9791ea59
MousumiPanda08 <sunupanda04@gmail.com>
2025-07-06 07:47:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 const bodyParser = require("body-parser");
 const session = require("express-session");
+const path = require("path");
 const { registerUser, authenticateUser } = require("./users");
 
 const app = express();
@@ -10,12 +11,7 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));  // Serve static files
-
-// Redirect root to login page
-app.get("/", (req, res) => {
-  res.redirect("/login.html");
-});
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(session({
     secret: 'chatsecret',
@@ -23,11 +19,39 @@ app.use(session({
     saveUninitialized: true
 }));
 
-app.post("/signup", (req, res) => { /* existing signup code */ });
-app.post("/login", (req, res) => { /* existing login code */ });
+app.get("/", (req, res) => {
+    res.redirect("/login.html");
+});
+
+app.post("/signup", (req, res) => {
+    const { username, password } = req.body;
+    if (registerUser(username, password)) {
+        res.send("Signup successful. Go back and login.");
+    } else {
+        res.status(400).send("Username already exists.");
+    }
+});
+
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    if (authenticateUser(username, password)) {
+        req.session.username = username;
+        res.redirect("/chat.html");
+    } else {
+        res.status(401).send("Invalid username or password.");
+    }
+});
 
 io.on("connection", (socket) => {
-    // existing socket logic...
+    socket.on("joinRoom", ({ username, room }) => {
+        socket.join(room);
+        socket.to(room).emit("message", `${username} joined ${room}`);
+    });
+
+    socket.on("chatMessage", (msg) => {
+        const rooms = Array.from(socket.rooms).filter(r => r !== socket.id);
+        rooms.forEach(room => socket.to(room).emit("message", msg));
+    });
 });
 
 const PORT = process.env.PORT || 3000;
